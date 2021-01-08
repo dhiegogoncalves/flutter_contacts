2f9cfc5be03f1412cfce910365c3e51970f79ff5
Dhiego Gonçalves da Silva <dhhiego@gmail.com>
2021-01-07 22:17:23 -0400

    Configuração do style e font.

--- lib/android/android.app.dart
+++ lib/android/android.app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_contacts/android/styles.dart';
 
 class AndroidApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -7,10 +8,7 @@ class AndroidApp extends StatelessWidget {
     return MaterialApp(
       title: 'Contacts',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
+      theme: androidTheme(),
       home: Scaffold(),
     );
   }
