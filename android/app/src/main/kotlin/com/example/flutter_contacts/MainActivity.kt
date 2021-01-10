a92e435da8cbc6362ff383634f8ab5131a4ce724
Dhiego Gonçalves da Silva <dhhiego@gmail.com>
2021-01-10 15:26:39 -0400

    Implementação do fingerprint.

--- android/app/src/main/kotlin/com/example/flutter_contacts/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_contacts/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.flutter_contacts
 
+import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
--- lib/android/views/splash.view.dart
+++ lib/android/views/splash.view.dart
@@ -1,6 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_contacts/android/views/home.view.dart';
+import 'package:flutter_contacts/controllers/auth.controller.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
+  @override
+  _SplashViewState createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  final controller = AuthController();
+
+  @override
+  void initState() {
+    controller.authenticate().then((result) {
+      if (result == true) {
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) => HomeView()),
+        );
+      }
+    }).catchError((error) {
+      print(error);
+    });
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
